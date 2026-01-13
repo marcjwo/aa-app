aec98a601e536e2d62af37be832df93cbc7842ee
marcjwo <marcwo@google.com>
2026-01-13 23:07:42 +0000

    visually working version, non functional sidebar

--- src/app/looker-embed/looker-embed.ts
+++ src/app/looker-embed/looker-embed.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject, OnChanges, SimpleChanges, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-looker-embed',
@@ -8,6 +9,32 @@ import { CommonModule } from '@angular/common';
   templateUrl: './looker-embed.html',
   styleUrl: './looker-embed.scss'
 })
-export class LookerEmbed {
+export class LookerEmbed implements OnChanges {
+  private sanitizer = inject(DomSanitizer);
+
   @Input() viewMode: 'dashboard' | 'report' = 'dashboard';
+  @Input() dashboardUrl: string = 'https://looker.cloud-bi-opm.com/embed/dashboards/349?Agent=%22real_time_agent%22&Date+Range=2025%2F01%2F01+to+2026%2F01%2F14';
+  @Input() reportUrl: string = 'https://lookerstudio.google.com:/embed/reporting/636ea626-6981-4043-b738-5657254666f8/page/8YdhF';
+
+  safeDashboardUrl: SafeResourceUrl | undefined;
+  safeReportUrl: SafeResourceUrl | undefined;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['dashboardUrl']) {
+      this.safeDashboardUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dashboardUrl);
+    }
+    if (changes['reportUrl']) {
+      this.safeReportUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.reportUrl);
+    }
+  }
+
+  // Initialize on start if inputs are already set
+  ngOnInit() {
+    if (!this.safeDashboardUrl && this.dashboardUrl) {
+      this.safeDashboardUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dashboardUrl);
+    }
+    if (!this.safeReportUrl && this.reportUrl) {
+      this.safeReportUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.reportUrl);
+    }
+  }
 }
